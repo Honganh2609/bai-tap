7331921ed05a66c3a9c30e2432872c943658c298
hong anh <mazic004@gmail.com>
2022-06-08 11:19:13 +0700

    update them nhap tien tinh lai

--- scripts.js
+++ scripts.js
@@ -11,6 +11,14 @@ let USDToVND = function() {
 }
 USDToVND()
 
+let compoundInterrest1 = function(){
+    let p = prompt("Số tiền bạn muốn tiết kiệm");
+    let n = prompt("Số năm bạn muốn gửi")
+    let tong = p * (1 + 0.07/1)**(1 * n);
+    console.log(`Tổng số tiền bạn nhận được sau ${n} năm là: ${tong}VND`)
+}
+compoundInterrest1()
+
 let compoundInterrest = function (P,r) {
     let A = P * (1 + r / 1)**(1 * 10);
     console.log (`Tiền lãi gửi theo năm sau 10 năm của bạn là: ${A}VND`);
